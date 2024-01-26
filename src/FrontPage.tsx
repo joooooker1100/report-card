10c8c1d56b9230b4a6b3a6eeccfd24c1c7bb436e
joooooker1100 <miladnazim10@gmail.com>
2024-01-27 03:14:48 +0330

    .

--- src/FrontPage.tsx
+++ src/FrontPage.tsx
@@ -2,11 +2,13 @@ import Islamic from './Islamic.png';
 import './App.css';
 
 function FrontPage() {
-
+  
   return (
     <div className="App">
+
       
         <div className="App-header">
+        
         <img src={Islamic} className="App-logo" alt="logo" />
 
         </div>
